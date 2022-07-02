4e391072c197ed3fb39f289d14f9ce8b649227cb
FelixHelixTM <108558974+FelixHelixTM@users.noreply.github.com>
2022-07-02 11:54:54 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,26 @@
+TP-Bingo
+=========
+
+Generator and item list for Twilight Princess Bingo. Based off of OoT bingo https://github.com/giuocob/OoT-Bingo
+
+Bingo uses a racefile, which is basically a file that has the world open from the start of the game.
+
+Setting up a racefile:
+A racefile can be made using the TP randomizer, go here for a guide on installing it: https://rando.zeldatp.net/
+
+Once you've done that, use the following settings to set up an un-randomized racefile:
+
+Page 2/11:
+Rando enabled? = No
+
+Page: 6/11:
+Set everything to No
+
+Page 7/11:
+Set everything to Yes
+
+Page 8/11:
+Set everything to Yes
+
+Page 9/11:
+Set GM Story flag and Start W/ Crstl to Yes, leave Hidden Skills as No
---
+++ js/goal-list.js
@@ -0,0 +1,352 @@
+var bingoList = [];
+bingoList["info"] = {
+version: "v1"
+};
+bingoList[1] = [
+        { name: "Die to a Swarm of Bees" },
+        { name: "Learn All 7 Hidden Skills" },
+        { name: "Rescue All Monkeys in Forest Temple" },
+        { name: "Defeat an Armos" },
+        { name: "Pour out Water" },
+        { name: "Get an Apple from Castle Town Market" },
+        { name: "Get Snowpeak Ruins Ordon Pumpkin" },
+        { name: "Clawshot to 5 Different Targets in One Dungeon" },
+        { name: "Defeat 3 Tileworms" }
+];
+bingoList[2] = [
+        { name: "Learn Mortal Draw" },
+        { name: "Get a Bottle" },
+        { name: "Get Spinner" },
+        { name: "Buy all Items in Death Mountain Goron Shop" },
+        { name: "Get City in the Sky Map, Compass & Boss Key" },
+        { name: "Defeat 5 Torch Slugs as wolf" },
+        { name: "Hit 3 Different Crystals within Dungeons" },
+        { name: "Get 8 Golden Bugs" },
+        { name: "Get Horse Call" },
+        { name: "Scoop up Lantern Oil" },
+        { name: "Defeat 5 White Wolfos" }
+];
+bingoList[3] = [
+        { name: "Get 4 Golden Bugs" },
+        { name: "Get Snowpeak Ruins Ooccoo" },
+        { name: "Get 10 Poe Souls" },
+        { name: "Defeat 2 Shell Blades with Water Bombs" },
+        { name: "Get all Goron Stockcave Checks" },
+        { name: "Talk to the Postman in 2 Different Locations" },
+        { name: "Get Forest Temple Ooccoo" },
+        { name: "Void Out in Faron Mist" },
+        { name: "Defeat 3 Moldorms" },
+        { name: "Get All 3 Goron Mines Small Keys" },
+        { name: "Complete 1 Level of Rollgoal" },
+        { name: "Get Temple of Time Ooccoo" }
+];
+bingoList[4] = [
+        { name: "Defeat Stallord" },
+        { name: "Defeat a Beamos with the Ball & Chain" },
+        { name: "Get a Fused Shadow" },
+        { name: "Talk to 5 Different Animals" },
+        { name: "Get 2 Sky Characters" },
+        { name: "Have a Fairy Revive You" },
+        { name: "Defeat 3 Enemies with the Temple of Time Statue" },
+        { name: "Score 60,000+ Points in Plumm Minigame" },
+        { name: "Pet All Goats in Ordon Ranch" },
+        { name: "Buy Coro Bottle" },
+        { name: "Defeat 5 Fire Bubbles" },
+        { name: "Get All 3 Temple of Time Small Keys" }
+];
+bingoList[5] = [
+        { name: "Open all Chests in Lakebed Temple" },
+        { name: "Get Light Sword" },
+        { name: "Bomb Yourself into Lava and Die" },
+        { name: "Refill Lantern Oil from a Bottle" },
+        { name: "Defeat Armogohma" },
+        { name: "Catch an Ordon Catfish" },
+        { name: "Get Wooden Sword" },
+        { name: "Defeat 5 Enemies with Slingshot" },
+        { name: "3 Tunics" },
+        { name: "Get a Bomb Bag" },
+        { name: "Get All 7 Palace of Twilight Small Keys" },
+        { name: "Get all Ordon Village Checks" }
+];
+bingoList[6] = [
+        { name: "Get 3 Sky Characters" },
+        { name: "Defeat 5 White Wolfos" },
+        { name: "Defeat Argorok" },
+        { name: "Get 2 Silver Rupees From Chests" },
+        { name: "Drink Great Fairy's Tears" },
+        { name: "Die While Wearing Zora Armor" },
+        { name: "Get Eaten by a Deku Like" },
+        { name: "Die to Your Own Bombs" },
+        { name: "Get Hawkeye" },
+        { name: "Donate 1,000 Rupees to Trill and Buy Nothing" },
+        { name: "Have Hena Kick You Out" },
+        { name: "Lose a Sol to a Zant Hand" }
+];
+bingoList[7] = [
+        { name: "Get 2 Bomb Bags" },
+        { name: "Die to a Darknut" },
+        { name: "Get Iron Boots" },
+        { name: "S.T.A.R. 2" },
+        { name: "Defeat Ook" },
+        { name: "Clear all Eldin Grottos" },
+        { name: "Defeat 5 Helmasaur" },
+        { name: "Defeat 5 Zant Heads" },
+        { name: "Clear all Lanayru Grottos" },
+        { name: "Sink in Quicksand" },
+        { name: "Defeat a Guay with the Slingshot" },
+        { name: "Examine all Sky Book Character Locations" },
+        { name: "Big/Giant Wallet" }
+];
+bingoList[8] = [
+        { name: "Get Fishing Rod" },
+        { name: "Hidden Village Cat Hide N' Seek" },
+        { name: "Get a Mirror Shard (from bosses)" },
+        { name: "Have 5 Hearts Total" },
+        { name: "Water a Pumpkin in Ordon Village" },
+        { name: "Drink Rare Chu Jelly" },
+        { name: "Shield Attack an Arrow" },
+        { name: "Get 5 Orange Rupees" },
+        { name: "Freeze to Death" },
+        { name: "Defeat a Big Baba" },
+        { name: "Get a Love Hint from Fanadi" },
+        { name: "Defeat all Aribter's Grounds Poes" },
+        { name: "Get 2 Fused Shadows" }
+];
+bingoList[9] = [
+        { name: "Defeat an Enemy by Knocking them into a Void" },
+        { name: "Get Forest Temple Map, Compass & Boss Key" },
+        { name: "Clear 10 Floors of Cave of Ordeals" },
+        { name: "Defeat 5 Blue Bokoblins" },
+        { name: "Get Ordon Shield" },
+        { name: "Play Fetch with a Dog" },
+        { name: "Defeat 5 Enemies with Back Slice" },
+        { name: "Visit All Provinces of the Map" },
+        { name: "Learn 2 Hidden Skills" },
+        { name: "Get All 3 Fused Shadows" },
+        { name: "Buy from all Gorons in Castle Town" },
+        { name: "Defeat a Bombskit with a Bombling" }
+];
+bingoList[10] = [
+        { name: "Get all Items Available via Flight by Fowl in Lake Hylia" },
+        { name: "Defeat 2 Arbiter's Grounds Poes" },
+        { name: "Complete 3 Levels of Rollgoal" },
+        { name: "Defeat 5 Chillfos" },
+        { name: "Get a Career Hint from Fanadi" },
+        { name: "Defeat 5 Red Bokoblins" },
+        { name: "Defeat DarkHammer" },
+        { name: "Use Purple Chu Jelly" },
+        { name: "Drown" },
+        { name: "Burn a Wooden or Ordon Shield" },
+        { name: "Polish Your Shoes in Castle Town" },
+        { name: "Defeat 3 Enemies with Spinner" },
+        { name: "Open the Temple of Time Door" },
+        { name: "Ride on Spinner Tracks in 2 Different Dungeons" }
+];
+bingoList[11] = [                                                                                  
+        { name: "Defeat an Enemy with Pots/Barrels /Boxes/etc" },
+        { name: "Turn in 20 Poe Souls to Jovanni" },
+        { name: "Defeat 3 Stalfos with Ball & Chain" },
+        { name: "Get all Faron Woods Checks" },
+        { name: "Get Arbiter's Grounds Ooccoo" },
+        { name: "Open all Lanayru Spring Chests" },
+        { name: "Get Dominion Rod" },
+        { name: "Unlock All Portals" },
+        { name: "Turn in 4 Bugs to Agitha" },
+        { name: "Buy Everything in Malo Mart (Kakariko)" },
+        { name: "Defeat 10 Poes" },
+        { name: "Defeat Dangoro" }
+];
+bingoList[12] = [
+        { name: "Donate to Charlo to get Item" },
+        { name: "Catch a Hyrule Bass" },
+        { name: "Fall Into a Pit" },
+        { name: "Get a Flower from Castle Town Market" },
+        { name: "Defeat 5 Enemies with Bombs" },
+        { name: "Talk to all Castle Town Pedestrians" },
+        { name: "Take Control of a Cucco" },
+        { name: "Get All 4 Forest Temple Small Keys" },
+        { name: "Defeat 10 Enemies with Mortal Draw" },
+        { name: "Score 30 Points in Iza's Minigame" },
+        { name: "Howl at Hidden Village Stone" },
+        { name: "Get 4 Heart Containers" }
+];
+bingoList[13] = [
+        { name: "Get Giant Bomb Bags" },
+        { name: "Defeat Temple of Time Darknut" },
+        { name: "Get all Lake Hylia Checks" },
+        { name: "Buy all Items in City in the Sky Oocca Shop" },
+        { name: "Ride the Zora up Zora's Domain Waterfall" },
+        { name: "Get Double Clawshots" },
+        { name: "Get a Green Rupee" },
+        { name: "Break 10 Different Pots or Boxes within Dungeons" },
+        { name: "Talk to Coro, Hena, and Iza" },
+        { name: "Get Yeeted Off Epona while Wearing Magic Armor" },
+        { name: "Open all City in the Sky Chests" },
+        { name: "Get & Equip Magic Armor" }
+];
+bingoList[14] = [
+        { name: "Get 2 of Every Rupee Type" },
+        { name: "Take Shortcut from South Castle Town to Lake Hylia" },
+        { name: "Drink Hot Springwater" },
+        { name: "Defeat a Beamos" },
+        { name: "Iza 1" },
+        { name: "Learn Ending Blow" },
+        { name: "Defeat an Armos with the Clawshot" },
+        { name: "Defeat a Beamos with Bombs" },
+        { name: "Defeat a Dodongo with a Bomb" },
+        { name: "Get 2 Heart Containers" },
+        { name: "Defeat Morpheel" },
+        { name: "Get 3 Mirror Shards (from bosses)" },
+        { name: "Score 0 Points in Iza's Minigame" }
+];
+bingoList[15] = [
+        { name: "Scoop up Blue Chu Jelly" },
+        { name: "Get Launched by a Goron" },
+        { name: "Defeat 5 Ice Bubbles" },
+        { name: "Drain All Your Rupees to Magic Armor" },
+        { name: "Catch a Hylian Pike" },
+        { name: "Turn the Lakebed Temple Stairs 5 Times" },
+        { name: "Die to a Floor Spike" },
+        { name: "Defeat Blizzeta" },
+        { name: "Defeat 30 Poes" },
+        { name: "Burn to Death" },
+        { name: "Take Control of a Cucco in Ordon Village Village, Kakariko Village & Lake Hylia" },
+        { name: "Talk to Telma (not a cutscene)" },
+        { name: "Rescue All Monkeys in Forest Temple" }
+];
+bingoList[16] = [
+        { name: "Shoot Down All 4 Paintings in Hyrule Castle" },
+        { name: "Defeat 3 Enemies with Iron Boots" },
+        { name: "Defeat 5 Lizalfos" },
+        { name: "Defeat 3 Freezards (and their Mini-Freezards)" },
+        { name: "Use Yellow Chu Jelly" },
+        { name: "Canoe Around Fishing Pond Rock Formation" },
+        { name: "Die to Electricity" },
+        { name: "Clear all Desert Grottos" },
+        { name: "Get all Kakariko Village Checks" },
+        { name: "Get some Bread from Castle Town Market" },
+        { name: "Drink Nasty Soup" },
+        { name: "Defeat both Phantom Zants" },
+        { name: "Buy Everything in Malo Mart (Castle Town)" }
+];
+bingoList[17] = [
+        { name: "Complete 5 Levels of Rollgoal" },
+        { name: "Dig up Green, Blue, and Yellow Rupees" },
+        { name: "Defeat 5 Enemies with Helm Splitter" },
+        { name: "Defeat Deku Toad" },
+        { name: "Learn 3 Hidden Skills" },
+        { name: "Defeat a ReDead with Bomb Arrows" },
+        { name: "Get Lantern" },
+        { name: "Catch a Fish Using Worms as Bait" },
+        { name: "Get Ball & Chain" },
+        { name: "Jump Off the Watch Tower and Die!" },
+        { name: "Get All 3 Lakebed Temple Small Keys" }
+];
+bingoList[18] = [
+        { name: "Get Forest Temple Map, Compass & Boss Key" },
+        { name: "Fly an Ooccoo into a Void" },
+        { name: "Defeat City in the Sky Aeralfos" },
+        { name: "Defeat all Snowpeak Ruins Poes" },
+        { name: "Get 2 Bomb Bags" },
+        { name: "Get all Hylia Bridge Checks" },
+        { name: "Howl at Upper Zora River Stone" },
+        { name: "Defeat Death Sword" },
+        { name: "Touch Bottom of Lake Hylia with Magic Armor On" },
+        { name: "Get Wooden Statue" },
+        { name: "Get Goron Mines Ooccoo" },
+        { name: "Get Master Sword" }
+];
+bingoList[19] = [
+        { name: "S.T.A.R. 1" },
+        { name: "Get a Sky Character" },
+        { name: "Iza 2" },
+        { name: "Get City in the Sky Ooccoo" },
+        { name: "Talk to all Goron Mines Elders" },
+        { name: "Get Lakebed Temple Map, Compass & Boss Key" },
+        { name: "Defeat 20 Poes" },
+        { name: "Plumm Minigame" },
+        { name: "Catch a Greengill" },
+        { name: "Defeat a Keese with Ball & Chain" },
+        { name: "Get the City in the Sky Small Key" },
+        { name: "Light 5 Different Torches" },
+];
+bingoList[20] = [
+        { name: "Clawshot to 10 Different Targets" },
+        { name: "Defeat a Toadpoli with Ball & Chain" },
+        { name: "Get Wooden Shield" },
+        { name: "Bonk into 5 Different Trees" },
+        { name: "Light a Torch and Extinquish it 3 Times in a Row" },
+        { name: "Get 2 Bottles" },
+        { name: "Visit the Hot Springs in Kakariko and Death Mountain" },
+        { name: "Defeat 3 Deku Likes" },
+        { name: "Open all Palace of Twilight Chests" },
+        { name: "Get Reekfish Scent" },
+        { name: "Defeat 5 Enemies with Great Spin" }
+];
+bingoList[21] = [
+        { name: "Press 5 Different Switches within Dungeons" },
+        { name: "Get Poe Scent" },
+        { name: "Defeat King Bulblin Twice" },
+        { name: "Defeat a Helmasaur with its own Armor" },
+        { name: "Get 5 Sky Characters" },
+        { name: "Get Ashei's Sketch" },
+        { name: "Die in the S.T.A.R. Minigame" },
+        { name: "Raid Hyrule Castle Treasure Room" },
+        { name: "Defeat Diababa" },
+        { name: "Get a Clawshot" },
+        { name: "Drink Red Chu Jelly" },
+        { name: "Catch 2 Different Fish with Hena" }
+];
+bingoList[22] = [
+        { name: "Get a Heart Container" },
+        { name: "Get Goron Mines Map, Compass & Boss Key" },
+        { name: "Defeat all City in the Sky Poes" },
+        { name: "Defeat 3 Enemies with a Bomb in the Boomerang" },
+        { name: "Defeat 10 Enemies as Wolf" },
+        { name: "Open all Chests in Goron Mines" },
+        { name: "Get Powered Dominion Rod" },
+        { name: "Get a Game Over" },
+        { name: "Defeat 5 ReDeads" },
+        { name: "Break 10 Suits of Armor in Snowpeak Ruins" },
+        { name: "Defeat 5 Enemies with Bow" }
+];
+bingoList[23] = [
+        { name: "Toss All Lake Hylia Cuccos into the Lake" },
+        { name: "Look Around in Hena's House (get all dialogues)" },
+        { name: "Catch a Fish Using Bee Larvae as Bait" },
+        { name: "Auto-Transform in Palace of Twilight Fog" },
+        { name: "Pull 2 Different Chains within Dungeons" },
+        { name: "Defeat 2 Dynalfos" },
+        { name: "Howl at Snowpeak Stone" },
+        { name: "Defeat 1 of Each Type of Chu" },
+        { name: "Get Hylian Shield" },
+        { name: "Fire a Snowpeak Ruins Cannon" },
+        { name: "Throw Hot Springwater Barrel on Ground Next to Thirsty Goron" }
+];
+bingoList[24] = [
+        { name: "Defeat Fyrus" },
+        { name: "Complete 4 Levels of Rollgoal" },
+        { name: "Defeat Zant" },
+        { name: "Defeat an Enemy With Bow Using Hawkeye" },
+        { name: "Turn in 12 Bugs to Agitha" },
+        { name: "Get Gale Boomerang" },
+        { name: "Knock Down 2 Bee Hives" },
+        { name: "Have 10 Hearts Total" },
+        { name: "Defeat 4 Shell Blades" },
+        { name: "Clear 30 Floors of Cave of Ordeals" },
+        { name: "Get All 4 Arbiter's Grounds Small Keys" },
+        { name: "Get Hero's Bow" }
+];
+bingoList[25] = [
+        { name: "Open all Chests in Arbiter's Grounds" },
+        { name: "Drown While Wearing Magic Armor" },
+        { name: "Defeat 10 Enemies with Ball & Chain" },
+        { name: "Stun 5 Enemies with Boomerang" },
+        { name: "Break a Floor Spike with the Spinner" },
+        { name: "Defeat all Temple of Time Poes" },
+        { name: "Scoop up Red Chu Jelly" },
+        { name: "Break All 21 Suits of Armor in Hyrule Castle" },
+        { name: "Get 12 Golden Bugs" },
+        { name: "Get All 3 Hyrule Castle Small Keys" },
+        { name: "Learn All 7 Hidden Skills" },
+];
